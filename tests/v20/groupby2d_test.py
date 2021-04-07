a28ce5d3a67f22d767e557d6bc38970fbad0da51
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-04-07 09:49:45 +0100

    Test operation a little better

--- tests/v20/groupby2d_test.py
+++ tests/v20/groupby2d_test.py
@@ -25,9 +25,7 @@ def test_groupby2d_simple_case_neutron_specific():
 
 
 def _make_simple_dataset(u, v, w):
-    size = np.prod((u, v, w))
-    data = sc.array(dims=['u', 'v', 'w'],
-                    values=np.arange(size).reshape((u, v, w)))
+    data = sc.array(dims=['u', 'v', 'w'], values=np.ones((u, v, w)))
     u = sc.array(dims=['u'], values=np.arange(u))
     v = sc.array(dims=['v'], values=np.arange(v))
     w = sc.array(dims=['w'], values=np.arange(w))
@@ -48,6 +46,16 @@ def test_simple_case_any_naming():
                         z='u',
                         preserve=[])
     assert grouped['a'].shape == [2, 5, 5]
+    projection = sc.array(dims=['v', 'w'], values=np.ones((5, 5))) * 4
+    expected_data = sc.reshape(sc.concatenate(projection, projection, dim='u'),
+                               sizes={
+                                   'u': 2,
+                                   'v': 5,
+                                   'w': 5
+                               })
+    assert sc.all(
+        sc.isclose(grouped['a'].data, expected_data,
+                   atol=1e-14 * sc.units.one)).value
 
 
 def test_groupby2d_different_output_size():
