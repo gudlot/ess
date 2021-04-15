ad377ad5dbad30d60b7a7a3ab85d2f3935a5505f
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-04-15 16:03:49 +0200

    yapf init

--- src/ess/reflectometry/__init__.py
+++ src/ess/reflectometry/__init__.py
@@ -1,11 +1,7 @@
 import scipp as sc
 from scipy.constants import neutron_mass, h, g
 
-h = (
-    h
-    * 1e20
-    / 1e6
-    * (sc.units.kg * sc.units.angstrom * sc.units.angstrom / sc.units.us)
-)
+h = (h * 1e20 / 1e6 *
+     (sc.units.kg * sc.units.angstrom * sc.units.angstrom / sc.units.us))
 HDM = h / (neutron_mass * sc.units.kg)
 G_ACC = -g * (sc.units.m / (sc.units.s * sc.units.s))
