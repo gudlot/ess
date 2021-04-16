de7b8dec268aa5cf622c426ce68159112eb66bf0
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-04-16 09:03:32 +0200

    generalise resolution

--- src/ess/reflectometry/resolution.py
+++ src/ess/reflectometry/resolution.py
@@ -8,21 +8,21 @@ import numpy as np
 import scipp as sc
 
 
-def detector_resolution(spatial_resolution, z_pixel_position,
-                        z_sample_position):
+def detector_resolution(spatial_resolution, pixel_position,
+                        sample_position):
     """
     Calculation the resolution function due to the spatial resolution of the detector.
 
     Args:
         spatial_resolution (`sc.Variable`): Detector spatial resolution.
-        z_pixel_position (`sc.Variable`): The position of each pixel in the z-dimension.
-        z_sample_position (`sc.Variable`): The position of the sample in the z-dimension.
+        pixel_position (`sc.Variable`): The position of each pixel in the dimension parallel to the beam.
+        sample_position (`sc.Variable`): The position of the sample in the dimension parallel to the beam.
 
     Returns:
         (`sc.Variable`): Standard deviation of detector resolution.
     """
     fwhm = sc.to_unit(
-        sc.atan(spatial_resolution / (z_pixel_position - z_sample_position)),
+        sc.atan(spatial_resolution / (pixel_position - sample_position)),
         "deg",
     )
     return fwhm / (2 * np.sqrt(2 * np.log(2)))
