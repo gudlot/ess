3748df98d7d5fce71f81fbfcb47f077618ffa1e2
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-04-23 15:29:53 +0100

    Make and use version information
    Take version information from git tag
    build.sh in conda uses git extracted version info to write special
    _fixed_version.py file used for packaging (gives future git indpendence)
    __init__.py calls _version.py to get version info. _version.py is
    generic and decides how to get _version info (git or file)

---
+++ src/ess/_version.py
@@ -0,0 +1,20 @@
+import os
+
+
+def _version():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    git_root = os.path.join(dir_path, '..', '..')
+    try:
+        import git
+        g = git.cmd.Git(git_root)
+        g.fetch()
+        return g.describe('--tags')
+    except (ImportError, git.exc.InvalidGitRepositoryError):
+        from . import _fixed_version
+        return _fixed_version.__version__
+
+
+if __name__ == "__main__":
+    print(_version())
+else:
+    __version__ = _version()
