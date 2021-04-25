d19fe1d66fe248924e2ac0f1b3152ecbf0016e42
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-04-25 19:24:41 +0100

    Attempted windows _fixed_version.py file construction

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,1 +1,6 @@
+@echo off
+echo | set /p="__version__" > _fixed_version.py
+echo | set /p="=" >> _fixed_version.py
+echo | set /p=$(python src/ess/_version.py) >> _fixed_version.py
+move _fixed_version.py src\ess
 move src\ess %CONDA_PREFIX%\lib\
