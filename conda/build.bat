01e471237d50cdcaf50c20f63ffd58d04c80677c
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-04-26 10:41:39 +0100

    Remove old bat file

--- conda/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo __version__ = '"'%undefined%'"' REM UNKNOWN: {"type":"Redirect","op":{"text":">","type":"great"},"file":{"text":"_fixed_version.py","type":"Word"}}
-COPY  _fixed_version.py src\ess\
-COPY  src\ess "%CONDA_PREFIX%"\lib\
