980ad99ded015be05ad2974b502e5122e585dd1b
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-04-26 10:39:57 +0100

    correct bld.bat for version writing

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,6 +1,8 @@
 @echo off
-echo | set /p="__version__" > _fixed_version.py
-echo | set /p="=" >> _fixed_version.py
-echo | set /p=$(python src/ess/_version.py) >> _fixed_version.py
+echo | set /p="__version__ = '" > _fixed_version.py
+python _version.py > output_ver
+set /p version=<output_ver
+echo | set /p=%version% >> _fixed_version.py
+del output_ver
 move _fixed_version.py src\ess
 move src\ess %CONDA_PREFIX%\lib\
