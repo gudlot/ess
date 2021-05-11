f845ee953b3e99866991ec7b53ec702c4179ec10
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-05-11 11:07:57 +0200

    missing linebreak

--- src/ess/amor/tools.py
+++ src/ess/amor/tools.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
-# flake8: noqa: E501"""
+# flake8: noqa: E501
+"""
 Tools to help with Amor data reduction.
 """
 
