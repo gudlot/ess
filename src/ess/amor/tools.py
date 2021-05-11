17068238bf738b388c86f097b4ca071cfc20785d
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-05-11 11:07:07 +0200

    add licence id

--- src/ess/amor/tools.py
+++ src/ess/amor/tools.py
@@ -1,5 +1,6 @@
-# flake8: noqa: E501
-"""
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
+# flake8: noqa: E501"""
 Tools to help with Amor data reduction.
 """
 
