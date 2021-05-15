4192b12111194d47aa1c6acba78e8f31738f9348
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-05-15 11:57:36 +0200

    add license id

--- tests/amor/tools_test.py
+++ tests/amor/tools_test.py
@@ -1,4 +1,6 @@
-# flake8: noqa: E501
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
+# # flake8: noqa: E501
 """
 Tests for tools module
 """
