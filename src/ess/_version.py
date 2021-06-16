b3550b955f507422971209a74374828885c00015
Neil Vaytet <neil.vaytet@esss.se>
2021-06-16 16:56:04 +0200

    use shared pipelines, with python build_conda.py script and remove _fixed_version since gitpython is a dependency

--- src/ess/_version.py
+++ src/ess/_version.py
@@ -3,14 +3,10 @@ import os
 
 def _version():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    try:
-        import git
-        g = git.cmd.Git(dir_path)
-        g.fetch()
-        return g.describe('--tags')
-    except Exception:
-        from . import _fixed_version
-        return _fixed_version.__version__
+    import git
+    g = git.cmd.Git(dir_path)
+    g.fetch()
+    return g.describe('--tags')
 
 
 if __name__ == "__main__":
