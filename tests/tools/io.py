b1b02053faf91ff8d1dbcc0959193da504a603bc
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-06-17 14:47:10 +0100

    Add missing test tools

---
+++ tests/tools/io.py
@@ -0,0 +1,19 @@
+from contextlib import contextmanager
+import tempfile
+import os
+
+
+@contextmanager
+def file_location(file_name):
+    """
+    Make a file in a temp directory.
+    Attempts to remove the file
+    at that location following the end of the context.
+    """
+    tempdir = tempfile.gettempdir()
+    f = os.path.join(tempdir, file_name)
+    yield f
+    try:
+        os.remove(f)
+    except OSError:
+        pass
