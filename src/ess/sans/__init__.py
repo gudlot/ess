85cc63e9bd0b4c6d0cf7dea90d4a7709abbc6764
Simon Heybrock <simon.heybrock@esss.se>
2021-06-30 07:34:46 +0200

    Unify code and add reducer customization

--- src/ess/sans/__init__.py
+++ src/ess/sans/__init__.py
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 
-from .reduction import reduce_by_wavelength
+from .reduction import reduce_to_q
--- src/ess/sans/reduction.py
+++ src/ess/sans/reduction.py
@@ -1,19 +1,30 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
+import scipp as sc
 
 
-def reduce_by_wavelength(data, q_bins, groupby, wavelength_bands):
+def simple_reducer(*, dim):
+    return lambda x: sc.sum(x, dim=dim)
+
+
+def grouping_reducer(*, dim, group):
+    return lambda x: sc.groupby(x, group=group).sum(dim=dim)
+
+
+def reduce_to_q(data, *, q_bins, reducer, wavelength_bands=None):
     # TODO Backup of the coord is necessary until `convert` can keep original
     wavelength = data.coords['wavelength']
-    data = sc.neutron.convert(data, 'wavelength', 'Q',
-                              out=data)  # TODO no gravity yet
+    data = sc.neutron.convert(data, 'wavelength', 'Q', out=data)
+    if wavelength_bands is None:
+        data = sc.histogram(data, q_bins)
+        return reducer(data)
     data.coords['wavelength'] = wavelength
     bands = None
     for i in range(wavelength_bands.sizes['wavelength'] - 1):
         low = wavelength_bands['wavelength', i]
         high = wavelength_bands['wavelength', i + 1]
         band = sc.histogram(data['wavelength', low:high], q_bins)
-        band = sc.groupby(band, group=groupby).sum('spectrum')
+        band = reducer(band)
         bands = sc.concatenate(bands, band,
                                'wavelength') if bands is not None else band
     bands.coords['wavelength'] = wavelength_bands
