9196aebb11b363c4bdfa4e5985071fff7e40450a
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-07-01 15:06:09 +0100

    Prep for setting phase

--- tests/v20/wfm/frames_analytical_test.py
+++ tests/v20/wfm/frames_analytical_test.py
@@ -4,14 +4,25 @@ import numpy as np
 
 
 def _to_angular_frequency(f):
-    return (2.0 * np.pi * sc.units.rad) * f
+    return (2.0 * np.pi) * f
 
 
 def allclose(x, y):
     return sc.all(sc.isclose(x, y)).value
 
 
-def _single_chopper_beamline(window_opening_t, pulse_length, tdc=None):
+def _chopper_ang_freq(window_opening_t, window_size):
+    ratio_of_window = window_size / (np.pi * 2)
+    # Required operational frequency of chopper
+    chopper_frequency = _to_angular_frequency(ratio_of_window /
+                                              window_opening_t)
+    return chopper_frequency
+
+
+def _single_chopper_beamline(window_opening_t,
+                             pulse_length,
+                             window_size=np.pi / 4 * sc.units.rad,
+                             tdc=None):
     instrument = sc.Dataset()
     # single pixel set 10m down the beam
     instrument['position'] = sc.vector(value=[0, 0, 10], unit=sc.units.m)
@@ -24,21 +35,17 @@ def _single_chopper_beamline(window_opening_t, pulse_length, tdc=None):
     instrument['pulse_length'] = pulse_length
 
     # We now set out to engineer a single cutout to deliver a specified
-    # length sub-pulse. We do this by setting a single cutout of 45 deg
-    # (pi/8) and then calculating rotation speed
+    # length sub-pulse.
     window_opening_t = sc.to_unit(window_opening_t, sc.units.s)
-    window_size = np.pi / 4  # choose a windows size 45 degrees
-    ratio_of_window = window_size / (np.pi * 2)
-    # Required operational frequency of chopper
-    chopper_frequency = ratio_of_window / window_opening_t
-
-    instrument['angular_frequency'] = _to_angular_frequency(
-        sc.broadcast(chopper_frequency, dims=['chopper'], shape=[1]))
+    chopper_ang_frequency = _chopper_ang_freq(window_opening_t, window_size)
+    instrument['angular_frequency'] = sc.broadcast(chopper_ang_frequency,
+                                                   dims=['chopper'],
+                                                   shape=[1])
 
     blind_t = sc.to_unit(pulse_length, sc.units.s) - window_opening_t
     # For calculation simplicity to center cutout opening over center of pulse
     phase_offset_t = blind_t / 2
-    phase = 2 * np.pi * phase_offset_t * chopper_frequency
+    phase = phase_offset_t * chopper_ang_frequency
     instrument['phase'] = sc.array(dims=['chopper'],
                                    values=[phase.value],
                                    unit=sc.units.rad)
@@ -48,9 +55,8 @@ def _single_chopper_beamline(window_opening_t, pulse_length, tdc=None):
         dims=['frame'], values=[0], unit=sc.units.rad) * sc.ones(
             dims=['chopper'], shape=[1])
     # window closing angle
-    instrument['frame_end'] = sc.array(
-        dims=['frame'], values=[window_size], unit=sc.units.rad) * sc.ones(
-            dims=['chopper'], shape=[1])
+    instrument['frame_end'] = sc.ones(dims=['frame'], shape=[1]) * sc.ones(
+        dims=['chopper'], shape=[1]) * window_size
 
     instrument['tdc'] = sc.zeros(dims=['chopper'], shape=[
         1
