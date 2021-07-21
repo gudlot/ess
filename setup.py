85430314844516578e33f96ba1dd0b72b98c5d9f
Neil Vaytet <neil.vaytet@esss.se>
2021-07-21 14:50:05 +0200

    flake8

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup
+import setuptools
 
 with open("README.md", 'r') as fh:
     long_description = fh.read()
 
-setup(name='ess',
-      version='1.0',
-      description='Neutron scattering tools for ESS',
-      license="BSD-3-clause",
-      long_description=long_description,
-      author='Scipp contributors (https://github.com/scipp)',
-      url="https://github.com/scipp/ess",
-      packages=setuptools.find_packages("src"),
-      package_dir={"": "src"},
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: BSD License",
-          "Operating System :: OS Independent",
-      ])
+setuptools.setup(name='ess',
+                 version='1.0',
+                 description='Neutron scattering tools for ESS',
+                 license="BSD-3-clause",
+                 long_description=long_description,
+                 author='Scipp contributors (https://github.com/scipp)',
+                 url="https://github.com/scipp/ess",
+                 packages=setuptools.find_packages("src"),
+                 package_dir={"": "src"},
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: BSD License",
+                     "Operating System :: OS Independent",
+                 ])
