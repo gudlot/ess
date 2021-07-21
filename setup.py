c5581e36ab7d76344fa9a88a787ec74a3f7096a1
Neil Vaytet <neil.vaytet@esss.se>
2021-07-21 15:03:12 +0200

    write the _fixed_version as part of the setup

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
+
+import os
 import setuptools
+import sys
 
-with open("README.md", 'r') as fh:
-    long_description = fh.read()
 
-setuptools.setup(name='ess',
-                 version='1.0',
-                 description='Neutron scattering tools for ESS',
-                 license="BSD-3-clause",
-                 long_description=long_description,
-                 author='Scipp contributors (https://github.com/scipp)',
-                 url="https://github.com/scipp/ess",
-                 packages=setuptools.find_packages("src"),
-                 package_dir={"": "src"},
-                 classifiers=[
-                     "Programming Language :: Python :: 3",
-                     "License :: OSI Approved :: BSD License",
-                     "Operating System :: OS Independent",
-                 ])
+def find_packages():
+    # Write fixed version to file to avoid having gitpython as a hard
+    # dependency
+    sys.path.append(os.path.abspath('src'))
+    from ess._version import __version__ as v
+    with open(os.path.join('src', 'ess', '_fixed_version.py'), 'w') as f:
+        f.write(f'__version__ = \'{v}\'\n')
+    return setuptools.find_packages('src')
+
+
+setuptools.setup(name='ess', packages=find_packages(), package_dir={"": "src"})
