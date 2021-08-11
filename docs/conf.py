f012d7f50a7feacb5018d7132f3c8e0f4be36656
Neil Vaytet <neil.vaytet@esss.se>
2021-08-11 22:54:37 +0200

    logo name

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ html_theme = 'sphinx_rtd_theme'
 #
 html_theme_options = {'logo_only': True}
 
-html_logo = "_static/logo-large-v4.png"
+html_logo = "_static/logo.png"
 html_favicon = "_static/favicon.ico"
 
 html_static_path = ['_static']
