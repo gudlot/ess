7a3e852a04fed623222ceaf6f82d7cbbd6f8d0a8
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2021-10-20 15:02:59 +0200

    Mask loading simplified

--- src/ess/loki/sans2d/load_files.py
+++ src/ess/loki/sans2d/load_files.py
@@ -22,33 +22,11 @@ def load_mask(idf_filename, mask_file):
     return scn.load(filename=idf_filename, mantid_alg='LoadMask', mantid_args={'InputFile': mask_file})
 
 def load_masks(idf_filename, mask_files, sample, background, spectrum_size):
-
-    mask_1_xml = load_mask(idf_filename, mask_files[0])
-    mask_2_xml = load_mask(idf_filename, mask_files[1])
-    mask_3_xml = load_mask(idf_filename, mask_files[2])
-    mask_4_xml = load_mask(idf_filename, mask_files[3])
-    mask_5_xml = load_mask(idf_filename, mask_files[4])
-    mask_6_xml = load_mask(idf_filename, mask_files[5])
-    mask_7_xml = load_mask(idf_filename, mask_files[6])
-    mask_8_xml = load_mask(idf_filename, mask_files[7])
-    mask_9_xml = load_mask(idf_filename, mask_files[8])
-
-    sample.masks['mask_1_xml'] = mask_1_xml['spectrum',:spectrum_size].data
-    sample.masks['mask_2_xml'] = mask_2_xml['spectrum',:spectrum_size].data
-    sample.masks['mask_3_xml'] = mask_3_xml['spectrum',:spectrum_size].data
-    sample.masks['mask_4_xml'] = mask_4_xml['spectrum',:spectrum_size].data
-    sample.masks['mask_5_xml'] = mask_5_xml['spectrum',:spectrum_size].data
-    sample.masks['mask_6_xml'] = mask_6_xml['spectrum',:spectrum_size].data
-    sample.masks['mask_7_xml'] = mask_7_xml['spectrum',:spectrum_size].data
-    sample.masks['mask_8_xml'] = mask_8_xml['spectrum',:spectrum_size].data
-    sample.masks['mask_9_xml'] = mask_9_xml['spectrum',:spectrum_size].data
-
-    background.masks['mask_1_xml'] = mask_1_xml['spectrum',:spectrum_size].data
-    background.masks['mask_2_xml'] = mask_2_xml['spectrum',:spectrum_size].data
-    background.masks['mask_3_xml'] = mask_3_xml['spectrum',:spectrum_size].data
-    background.masks['mask_4_xml'] = mask_4_xml['spectrum',:spectrum_size].data
-    background.masks['mask_5_xml'] = mask_5_xml['spectrum',:spectrum_size].data
-    background.masks['mask_6_xml'] = mask_6_xml['spectrum',:spectrum_size].data
-    background.masks['mask_7_xml'] = mask_7_xml['spectrum',:spectrum_size].data
-    background.masks['mask_8_xml'] = mask_8_xml['spectrum',:spectrum_size].data
-    background.masks['mask_9_xml'] = mask_9_xml['spectrum',:spectrum_size].data
+    """
+    Loading masks files from the list and add them to sample and background
+    """
+    for i, mask_file in enumerate(mask_files):
+        mask_xml = load_mask(idf_filename, mask_file)
+        sample.masks[f'mask_{i}_xml'] = mask_xml['spectrum',:spectrum_size].data 
+        background.masks[f'mask_{i}_xml'] = mask_xml['spectrum',:spectrum_size].data
+    
