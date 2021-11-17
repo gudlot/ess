9dad31c7f0eb31ca7ffc0d64055375a3cfd544b6
Neil Vaytet <39047984+nvaytet@users.noreply.github.com>
2021-11-17 16:47:13 +0100

    Update src/ess/reflectometry/io/load.py
    Co-authored-by: Andrew McCluskey <andrew.mccluskey@ess.eu>

--- src/ess/reflectometry/io/load.py
+++ src/ess/reflectometry/io/load.py
@@ -8,7 +8,7 @@ import warnings
 
 
 def load(filename: str,
-         sample_angle_offset: sc.Variable = 0 * sc.units.deg,
+         omega: sc.Variable = 0 * sc.units.deg,
          beam_size: sc.Variable = 0.001 * sc.units.m,
          sample_size: sc.Variable = 0.01 * sc.units.m,
          detector_spatial_resolution: sc.Variable = 0.0025 * sc.units.m,
