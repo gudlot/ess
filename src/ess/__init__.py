44f9f9a11d845ee26228f252d1256b1058eab140
Neil Vaytet <neil.vaytet@esss.se>
2021-12-13 13:47:39 +0100

    add missing deps for building docs and change version setting to not depend on gitpython

--- src/ess/__init__.py
+++ src/ess/__init__.py
@@ -1,8 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
-from . import _version
 
-__version__ = _version.__version__
+try:
+    from . import _version
+    __version__ = _version.__version__
+except ImportError:
+    pass
 
 from . import amor
 from . import reflectometry
