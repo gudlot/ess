e23eab11441b7689b151cd8a3e7897b4cf376b76
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2021-12-21 12:03:05 +0100

    Notebook updates

--- src/ess/sans/__init__.py
+++ src/ess/sans/__init__.py
@@ -1,3 +1,3 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
-
+from . import conversions
---
+++ src/ess/sans/conversions.py
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
+
+import scipp as sc
+from scipp.constants import m_n, h, pi
+from scippneutron.tof import conversions
+from scippneutron.core.conversions import _elem_dtype, _elem_unit
+
+
+def two_theta(gravity, wavelength, incident_beam, scattered_beam):
+    grav = sc.norm(gravity)
+    L2 = sc.norm(scattered_beam)
+    y = sc.dot(scattered_beam, gravity) / grav
+    n = sc.cross(incident_beam, gravity)
+    n /= sc.norm(n)
+    x_term = sc.dot(scattered_beam, n)
+    x_term *= x_term
+
+    y_term = sc.to_unit(wavelength, _elem_unit(L2), copy=True)
+    y_term *= y_term
+    drop = L2**2
+    drop *= grav * (m_n**2 / (2 * h**2))
+    # Optimization when handling either the dense or the event coord of binned data:
+    # - For the event coord, both operands have same dims, and we can multiply in place
+    # - For the dense coord, we need to broadcast using non in-place operation
+    if set(drop.dims).issubset(set(y_term.dims)):
+        y_term *= drop
+    else:
+        y_term = y_term * drop
+    y_term += y
+    y_term *= y_term
+
+    if set(x_term.dims).issubset(set(y_term.dims)):
+        y_term += x_term
+    else:
+        y_term = y_term + x_term
+    out = sc.sqrt(y_term, out=y_term)
+    out /= L2
+    out = sc.asin(out, out=out)
+    return out
+
+
+def sans_elastic() -> dict:
+    """
+    Generate a coordinate transformation graph for SANS elastic scattering.
+    """
+    graph = {**conversions.beamline(scatter=True), **conversions.elastic_Q("tof")}
+    graph["two_theta"] = two_theta
+    return graph
