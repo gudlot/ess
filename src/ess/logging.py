80b08decbe63d6de8ce8a2108b4120aa8f9de52b
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-01-17 15:21:50 +0100

    Revert to stdlib logging

--- src/ess/logging.py
+++ src/ess/logging.py
@@ -6,13 +6,11 @@ import functools
 import logging.config
 import logging
 import inspect
-import re
 from os import PathLike
-from typing import Any, Callable, Literal, MutableMapping, Optional, Union
+from typing import Any, Callable, List, Literal, Optional, Union
 
 import scipp as sc
 import scippneutron as scn
-import structlog
 
 
 def get_logger(instrument: Optional[str] = None) -> logging.Logger:
@@ -96,7 +94,6 @@ def _make_stream_handler(level: Union[str, int]) -> logging.StreamHandler:
     return handler
 
 
-# TODO share handler?
 def _make_file_handler(filename: Union[str, PathLike],
                        level: Union[str, int]) -> logging.FileHandler:
     handler = logging.FileHandler(filename, mode='w')
@@ -107,23 +104,25 @@ def _make_file_handler(filename: Union[str, PathLike],
     return handler
 
 
-def _configure_root(level, filename, reset):
+def _make_widget_handler(level: Union[str, int]) -> sc.logging.WidgetHandler:
+    return sc.logging.WidgetHandler(level=level, widget=sc.logging.LogWidget())
+
+
+def _configure_root(handlers: List[logging.Handler], level: Union[str, int], reset):
     root = logging.getLogger()
     if reset:
-        for handler in root.handlers:
+        for handler in list(root.handlers):
             root.removeHandler(handler)
+        for filt in list(root.filters):
+            root.removeFilter(filt)
 
-    _configure_logger(root, filename, level)
+    _configure_logger(root, handlers, level)
 
 
-# TODO separate levels for handlers
-def _configure_logger(logger: logging.Logger, filename: Optional[Union[str, PathLike]],
+def _configure_logger(logger: logging.Logger, handlers: List[logging.Handler],
                       level: Union[str, int]):
-    logger.addHandler(_make_stream_handler(level))
-    if filename is not None:
-        logger.addHandler(_make_file_handler(filename, level))
-    # TODO make it work when not in Jupyter
-    logger.addHandler(sc.logging.get_widget_handler())
+    for handler in handlers:
+        logger.addHandler(handler)
     logger.setLevel(level)
 
 
@@ -138,21 +137,26 @@ def _thread_name_abbreviator(logger: logging.Logger, method_name: str,
             event_dict['thread_name'] = match[1]
     return event_dict
 
+def _make_handlers(filename: Optional[Union[str, PathLike]],
+                   file_level: Union[str, int], stream_level: Union[str, int],
+                   widget_level: Union[str, int]) -> List[logging.Handler]:
+    handlers = [_make_stream_handler(stream_level)]
+    if filename is not None:
+        handlers.append(_make_file_handler(filename, file_level))
+    if sc.utils.running_in_jupyter():
+        handlers.append(_make_widget_handler(widget_level))
+    return handlers
 
-def _key_value_inserter(logger, method_name, event_dict):
-    aux = dict(event_dict)
-    del aux['timestamp']
-    del aux['level']
-    del aux['logger']
-    del aux['event']
-    rend = structlog.processors.KeyValueRenderer()(logger, method_name, aux)
-    if rend:
-        event_dict['event'] += ' ' + rend
-    return event_dict
+
+def _base_level(levels: List[Union[str, int]]) -> int:
+    return min((logging.getLevelName(level) if isinstance(level, str) else level
+                for level in levels))
 
 
 def configure(filename: Optional[Union[str, PathLike]] = 'scipp.ess.log',
-              level: Union[str, int] = logging.INFO,
+              file_level: Union[str, int] = logging.INFO,
+              stream_level: Union[str, int] = logging.WARNING,
+              widget_level: Union[str, int] = logging.INFO,
               root: Union[bool, Literal['yes', 'no', 'overwrite']] = False):
     """Set up logging for the ess package.
 
@@ -163,93 +167,15 @@ def configure(filename: Optional[Union[str, PathLike]] = 'scipp.ess.log',
 
     TODO details
     """
-    timestamper = structlog.processors.TimeStamper(fmt='%Y-%m-%dT%H:%M:%S')
-    callsite_param_adder = structlog.processors.CallsiteParameterAdder(
-        {structlog.processors.CallsiteParameter.THREAD_NAME})
-    foreign_pre_chain = [
-        timestamper,
-        structlog.stdlib.add_log_level,
-        structlog.stdlib.add_logger_name,
-        structlog.stdlib.ExtraAdder(),
-        callsite_param_adder,
-        _thread_name_abbreviator,
-    ]
-    logging.config.dictConfig({
-        'version': 1,
-        'disable_existing_loggers': False,
-        'incremental': False,  # TODO good choice?
-        'formatters': {
-            'plain': {
-                '()': 'logging.Formatter',
-                # TODO thread?
-                'format': '[%(asctime)s] <%(name)s> %(levelname)-8s : %(message)s',
-                'datefmt': '%Y-%m-%dT%H:%M:%S',
-            },
-            'json': {
-                '()':
-                structlog.stdlib.ProcessorFormatter,
-                'processors': [
-                    # TODO currently prints both logging and structlog fields
-                    structlog.stdlib.ProcessorFormatter.remove_processors_meta,
-                    structlog.processors.JSONRenderer(),
-                ],
-                'foreign_pre_chain':
-                foreign_pre_chain,
-            },
-        },
-        'handlers': {
-            'stream': {
-                'level': 'INFO',
-                'class': 'logging.StreamHandler',
-                'formatter': 'plain',
-            },
-            'json_file': {
-                'level': 'INFO',
-                'class': 'logging.FileHandler',
-                'formatter': 'json',
-                'filename': 'test.log.json',
-            },
-        },
-        'loggers': {
-            '': {
-                'handlers': ['stream', 'json_file'],
-                'level': 'INFO',
-                'propagate': True,
-            }
-        }
-    })
-    structlog.configure(
-        processors=[
-            structlog.stdlib.filter_by_level,
-            # add timestamp, level, name even though logging handles them separately
-            # need them for the JSON formatter
-            timestamper,
-            structlog.stdlib.add_log_level,
-            structlog.stdlib.add_logger_name,
-            structlog.stdlib.PositionalArgumentsFormatter(),
-            structlog.processors.StackInfoRenderer(),
-            structlog.processors.format_exc_info,
-            structlog.processors.UnicodeDecoder(),
-            callsite_param_adder,
-            # _thread_name_abbreviator,
-            # structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
-            _key_value_inserter,
-            structlog.stdlib.render_to_log_kwargs,
-        ],
-        logger_factory=structlog.stdlib.LoggerFactory(),
-        wrapper_class=structlog.stdlib.BoundLogger,
-        cache_logger_on_first_use=True,
-    )
-
-    return
+    handlers = _make_handlers(filename, file_level, stream_level, widget_level)
+    base_level = _base_level([file_level, stream_level, widget_level])
     if root is True or root in ('yes', 'overwrite'):
-        _configure_root(level, filename, reset=root == 'overwrite')
+        _configure_root(handlers, base_level, reset=root == 'overwrite')
     # TODO don't configure twice -> update scipp
-    _configure_logger(sc.get_logger(), filename, level)
     import pooch
-    _configure_logger(pooch.get_logger(), filename, level)
+    for logger in (sc.get_logger(), pooch.get_logger(), logging.getLogger('Mantid')):
+        _configure_logger(logger, handlers, base_level)
     # TODO mantid's own config
-    _configure_logger(logging.getLogger('Mantid'), filename, level)
 
 
 def greet():
@@ -262,7 +188,3 @@ def greet():
         '''ess: %s (https://scipp.github.io/ess/)
 scippneutron: %s (https://scipp.github.io/scippneutron/)
 scipp: %s (https://scipp.github.io/)''', __version__, scn.__version__, sc.__version__)
-
-
-# TODO time zone in logs
-# TODO store file name in datasets?
