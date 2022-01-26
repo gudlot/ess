d2f1903393b9db97ce237a66a8d4d3ff79b1f8f0
Neil Vaytet <neil.vaytet@esss.se>
2022-01-26 16:31:56 +0100

    use pooch for data files

--- src/ess/loki/__init__.py
+++ src/ess/loki/__init__.py
@@ -1,4 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
 
+# flake8: noqa: F401
+
+from . import data
 from .io import load_sans2d, load_rkh_wav, load_rkh_q
