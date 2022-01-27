81d37b9605b409949b409c0a40e21143543d5d6f
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-01-27 17:18:55 +0100

    Smooth using FFT

--- src/ess/diffraction/powder/normalization.py
+++ src/ess/diffraction/powder/normalization.py
@@ -4,12 +4,15 @@
 import scipp as sc
 from scippneutron.tof.conversions import beamline, elastic
 
-from .smoothing import smooth_data
+from .smoothing import fft_smooth
 from .tools import unwrap_attr
 
 
-def normalize_by_monitor(data: sc.DataArray, *, monitor: str,
-                         wavelength_edges: sc.Variable) -> sc.DataArray:
+def normalize_by_monitor(data: sc.DataArray,
+                         *,
+                         monitor: str,
+                         wavelength_edges: sc.Variable,
+                         smooth_args=None) -> sc.DataArray:
     """
 
     """
@@ -24,13 +27,9 @@ def normalize_by_monitor(data: sc.DataArray, *, monitor: str,
                                    keep_inputs=False,
                                    keep_intermediate=False,
                                    keep_aliases=False)
-    mon = smooth_data(mon, dim='wavelength', NPoints=40)
 
     mon = sc.rebin(mon, 'wavelength', wavelength_edges)
-    # TODO why does this produce different values?
-    #   How do we know what is the correct binning?
-    # mi = wavelength_edges.min()
-    # ma = wavelength_edges.max()
-    # mon = mon['wavelength', mi:ma]
-
+    if smooth_args is not None:
+        print(f'Smoothing monitor {monitor} for normalisation with {smooth_args}.')
+        mon = fft_smooth(mon, dim='wavelength', **smooth_args)
     return data.bins / sc.lookup(func=mon, dim='wavelength')
