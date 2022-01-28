2dbe0eba5d102d5710e8f72929e4e890709e44d8
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-01-28 16:02:09 +0100

    Add subtract_empty_instrument

--- src/ess/diffraction/powder/normalization.py
+++ src/ess/diffraction/powder/normalization.py
@@ -33,3 +33,41 @@ def normalize_by_monitor(data: sc.DataArray,
         print(f'Smoothing monitor {monitor} for normalisation with {smooth_args}.')
         mon = fft_smooth(mon, dim='wavelength', **smooth_args)
     return data.bins / sc.lookup(func=mon, dim='wavelength')
+
+
+def _common_wavelength_edges(*data):
+    """
+    The data has wavelength bin edges for each spectrum:
+
+        ^      |---|
+    spectrum  |--|
+        v        |--|
+            < wavelength >
+
+    This function computes common edges for all spectra
+    for the combination of all inputs:
+
+        ^     | --- |
+    spectrum  |--   |
+        v     |   --|
+            < wavelength >
+
+    """
+    min_wavelength = sc.concat(
+        [d.coords['wavelength']['wavelength', 0].min() for d in data],
+        '_aux').min('_aux')
+    max_wavelength = sc.concat(
+        [d.coords['wavelength']['wavelength', 1].max() for d in data],
+        '_aux').min('_aux')
+    return sc.concat([min_wavelength, max_wavelength], 'wavelength')
+
+
+def subtract_empty_instrument(data, empty_instr):
+    data = data.copy(deep=False)
+    empty_instr = empty_instr.copy(deep=False)
+
+    edges = _common_wavelength_edges(data, empty_instr)
+    data.coords['wavelength'] = edges
+    empty_instr.coords['wavelength'] = edges
+
+    return data.bins.concat(-empty_instr)
