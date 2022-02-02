15c04e1130927ef87b97538340bf49d202161f67
Neil Vaytet <neil.vaytet@esss.se>
2022-02-02 16:15:26 +0100

    split q1d into smaller parts

--- src/ess/sans/__init__.py
+++ src/ess/sans/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
 
+from . import common
 from . import conversions
 from . import normalization
 from .q1d import q1d
--- src/ess/sans/q1d.py
+++ src/ess/sans/q1d.py
@@ -1,11 +1,155 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+
+from typing import Tuple
 import scipp as sc
 from scipp.constants import g
+from .common import gravity_vector
 from . import conversions
 from . import normalization
 from ..wfm.tools import to_bin_centers, to_bin_edges
 from scipp.interpolate import interp1d
+
+
+def _make_coordinate_transform_graphs(gravity):
+    """
+    Create unit conversion graphs
+    """
+    data_graph = conversions.sans_elastic(gravity=gravity)
+    monitor_graph = conversions.sans_monitor()
+    return data_graph, monitor_graph
+
+
+def _convert_to_wavelength(data: sc.DataArray, monitors: dict, data_graph: dict,
+                           monitor_graph: dict) -> Tuple[sc.DataArray, dict]:
+    """
+    Convert the data array and all the items inside the dict of monitors to wavelength.
+    The gravity parameter can be used to turn on or off the effects of gravity.
+    """
+
+    # if gravity:
+    #     # Make shallow copy of data so as to not add coords in-place
+    #     data = data.copy(deep=False)
+    #     data.coords["gravity"] = gravity_vector()
+
+    # # Create unit conversion graphs
+    # graph = conversions.sans_elastic(gravity=gravity)
+    # graph_monitor = conversions.sans_monitor()
+
+    # Convert to wavelength
+    data = data.transform_coords("wavelength", graph=data_graph)
+    monitors = {
+        key: monitors[key].transform_coords("wavelength", graph=monitor_graph)
+        for key in monitors
+    }
+
+    return data, monitors
+
+
+def _denoise_and_rebin_monitors(monitors: dict, wavelength_bins: sc.Variable,
+                                non_background_range: sc.Variable) -> dict:
+    """
+    Subtract a background baseline from monitor counts, taken as the mean of the counts
+    outside the specified ``non_background_range``.
+    """
+    return {
+        key: normalization.subtract_background_and_rebin(
+            monitors[key],
+            wavelength_bins=wavelength_bins,
+            non_background_range=non_background_range)
+        for key in monitors
+    }
+
+
+def _direct_beam(direct_beam: sc.DataArray,
+                 wavelength_bins: sc.Variable) -> sc.DataArray:
+    """
+    If the wavelength binning of the direct beam function does not match the requested
+    ``wavelength_bins``, perform a 1d interpolation of the function onto the bins.
+    """
+    if sc.identical(direct_beam.coords['wavelength'], wavelength_bins):
+        return direct_beam
+    func = interp1d(sc.values(direct_beam), 'wavelength')
+    direct_beam = func(wavelength_bins, midpoints=True)
+    logger = sc.get_logger()
+    logger.warning('An interpolation was performed on the direct_beam function. '
+                   'The variances in the direct_beam function have been ignored.')
+    return direct_beam
+
+
+def _denominator(direct_beam: sc.DataArray, data_incident_monitor: sc.DataArray,
+                 transmission_fraction: sc.DataArray,
+                 solid_angle: sc.Variable) -> sc.DataArray:
+    """
+    Compute the denominator term.
+    Because we are histogramming the Q values of the denominator further down in the
+    workflow, we convert the wavelength coordinate of the denominator from bin edges to
+    bin centers.
+    """
+    denominator = (solid_angle * direct_beam * data_incident_monitor *
+                   transmission_fraction)
+    # TODO: once scipp-0.12 is released, use sc.midpoints()
+    denominator.coords['wavelength'] = 0.5 * (denominator.coords['wavelength'][1:] +
+                                              denominator.coords['wavelength'][:-1])
+    return denominator
+
+
+def _convert_to_q_and_merge_spectra(data, graph, wavelength_bands, gravity):
+    """
+    Convert the data to momentum vector Q. This accepts both dense and event data.
+    The final step merges all spectra:
+      - In the case of event data, events in all bins are concatenated
+      - In the case of dense data, counts in all spectra are summed
+    """
+    if gravity:
+        data = data.copy(deep=False)
+        data.coords["gravity"] = gravity_vector()
+
+    if data.bins is not None:
+        out = _convert_events_to_q_and_merge_spectra(data=data,
+                                                     graph=graph,
+                                                     q_bins=q_bins,
+                                                     wavelength_bands=wavelength_bands)
+    else:
+        out = _convert_dense_to_q_and_merge_spectra(data=data,
+                                                    graph=graph,
+                                                    q_bins=q_bins,
+                                                    wavelength_bands=wavelength_bands)
+    if wavelength_bands.sizes['wavelength'] == 2:
+        out = out['wavelength', 0]
+    return out
+
+
+def _convert_events_to_q_and_merge_spectra(data, graph, q_bins, wavelength_bands):
+    data = data.transform_coords("Q", graph=graph)
+    q_boundaries = sc.concat([q_bins.min(), q_bins.max()], dim='Q')
+
+    # TODO: once scipp-0.12 is out, we no longer need to move the attr into the coords
+    data.bins.coords['wavelength'] = data.bins.attrs.pop('wavelength')
+
+    q_binned = sc.bin(data, edges=[wavelength_bands, q_boundaries])
+    q_summed = q_binned.bins.concat('spectrum')
+    return q_summed
+
+
+def _convert_dense_to_q_and_merge_spectra(data, graph, q_bins, wavelength_bands):
+    bands = []
+    print(wavelength_bands.sizes['wavelength'])
+    for i in range(wavelength_bands.sizes['wavelength'] - 1):
+        band = data['wavelength', wavelength_bands[i]:wavelength_bands[i + 1]]
+        q_band = band.transform_coords("Q", graph=graph)
+        bands.append(sc.histogram(q_band, bins=q_bins))
+        # bands.append(sc.histogram(q_band, bins=q_bins).sum('spectrum'))
+    q_summed = sc.concat(bands, 'wavelength').sum('spectrum')
+    # q_summed = sc.concat(denominator_bands, 'wavelength')
+    return q_summed
+
+
+def _normalize(numerator, denominator, dim='Q'):
+    if numerator.bins is not None:
+        return numerator.bins / sc.lookup(func=denominator, dim=dim)
+    else:
+        return numerator / denominator
 
 
 def q1d(data,
@@ -16,14 +160,9 @@ def q1d(data,
         direct_beam,
         wavelength_bins,
         q_bins,
-        number_of_wavelength_bands=1,
-        monitor_background_threshold=sc.scalar(30.0, unit='counts'),
-        histogram_output=True):
-
-    # 1. Convert to wavelength =========================================================
-
-    # Make shallow copy of data so as to not add coords in-place
-    data = data.copy(deep=False)
+        gravity=False,
+        monitor_non_background_range=None,
+        wavelength_bands=None):
 
     monitors = {
         'data_incident_monitor': data_incident_monitor,
@@ -32,97 +171,78 @@ def q1d(data,
         'direct_transmission_monitor': direct_transmission_monitor
     }
 
-    # Create unit conversion graphs
-    graph = conversions.sans_elastic()
-    graph_monitor = conversions.sans_monitor()
+    data_graph, monitor_graph = _make_coordinate_transform_graphs(gravity=gravity)
 
-    # Add gravity coordinate
-    data.coords["gravity"] = sc.vector(value=[0, -1, 0]) * g
+    data, monitors = _convert_to_wavelength(data=data,
+                                            monitors=monitors,
+                                            data_graph=data_graph,
+                                            monitor_graph=monitor_graph)
 
-    # Convert to wavelength
-    data = data.transform_coords("wavelength", graph=graph)
-    monitors_wav = {
-        key: monitors[key].transform_coords("wavelength", graph=graph_monitor)
-        for key in monitors
-    }
+    monitors = _denoise_and_rebin_monitors(monitors=monitors,
+                                           wavelength_bins=wavelength_bins,
+                                           non_background_range=non_background_range)
 
-    # Align monitors to common wavelength axis
-    for key in monitors_wav:
-        monitors_wav[key] = normalization.subtract_background_and_rebin(
-            monitors_wav[key],
-            wavelength_bins=wavelength_bins,
-            threshold=monitor_background_threshold)
+    transmission_fraction = normalization.transmission_fraction(**monitors)
 
-    # 2. Compute denominator ===========================================================
+    solid_angle = normalization.solid_angle_of_rectangular_pixels(
+        data,
+        pixel_width=data.coords['pixel_width'],
+        pixel_height=data.coords['pixel_height'])
 
-    # Transmission fraction
-    transmission_fraction = normalization.transmission_fraction(**monitors_wav)
+    direct_beam = _direct_beam(direct_beam=direct_beam, wavelength_bins=wavelength_bins)
 
-    # Solid angle
-    solid_angle = normalization.solid_angle(data,
-                                            pixel_width=data.coords['pixel_width'],
-                                            pixel_height=data.coords['pixel_height'])
-
-    # Interpolate the direct beam function to the requested wavelength binning
-    interpolate_direct_beam = True
-    if direct_beam.coords['wavelength'].sizes['wavelength'] > direct_beam.sizes[
-            'wavelength']:
-        if sc.identical(direct_beam.coords['wavelength'], wavelength_bins):
-            interpolate_direct_beam = False
-    else:
-        if sc.identical(
-                to_bin_edges(direct_beam.coords['wavelength'], dim='wavelength'),
-                wavelength_bins):
-            interpolate_direct_beam = False
-    if interpolate_direct_beam:
-        func = interp1d(sc.values(direct_beam), 'wavelength')
-        direct_beam = func(wavelength_bins, midpoints=True)
-        logger = sc.get_logger()
-        logger.warning('ess.sans.q1d: An interpolation was performed on the '
-                       'direct_beam function. The variances in the direct_beam '
-                       'function have been ignored.')
-
-    # Compute denominator and copy coords needed for conversion to Q
-    denominator = (solid_angle * direct_beam * monitors_wav['data_incident_monitor'] *
-                   transmission_fraction)
-    denominator.coords['position'] = data.meta['position']
-    denominator.coords['gravity'] = data.meta['gravity']
-    denominator.coords['sample_position'] = data.meta['sample_position']
-    denominator.coords['source_position'] = data.meta['source_position']
-    denominator.coords['wavelength'] = to_bin_centers(denominator.coords['wavelength'],
-                                                      dim='wavelength')
+    denominator = _denominator(direct_beam=direct_beam,
+                               data_incident_monitor=monitors['data_incident_monitor'],
+                               transmission_fraction=transmission_fraction,
+                               solid_angle=solid_angle)
+    # Insert copy coords needed for conversion to Q.
+    # TODO: can this be avoided by copying the Q coords from the converted numerator?
+    for coord in ['position', 'sample_position', 'source_position']:
+        denominator.coords[coord] = data.meta[coord]
 
     # 3. Convert from wavelength to Q ==================================================
 
-    wavelength_bands = sc.linspace(dim='wavelength',
-                                   start=wavelength_bins.min().value,
-                                   stop=wavelength_bins.max().value,
-                                   num=number_of_wavelength_bands + 1,
-                                   unit=wavelength_bins.unit)
+    # In the case where no wavelength bands are requested, we create a single wavelength
+    # band to make sure we select the correct wavelength range that corresponds to
+    # wavelength_bins
+    if wavelength_bands is None:
+        wavelength_bands = sc.concat(
+            [wavelength_bins.min(), wavelength_bins.max()], dim='wavelength')
 
-    data = data.transform_coords("Q", graph=graph)
-    q_boundaries = sc.concat([q_bins.min(), q_bins.max()], dim='Q')
+    data_q = _convert_to_q_and_merge_spectra(data=data,
+                                             graph=data_graph,
+                                             wavelength_bands=wavelength_bands,
+                                             gravity=gravity)
 
-    # TODO: once scipp-0.12 is out, we no longer need to move the attr into the coords
-    data.bins.coords['wavelength'] = data.bins.attrs.pop('wavelength')
+    denominator_q = _convert_to_q_and_merge_spectra(data=denominator,
+                                                    graph=data_graph,
+                                                    wavelength_bands=wavelength_bands,
+                                                    gravity=gravity)
 
-    q_binned = sc.bin(data, edges=[wavelength_bands, q_boundaries])
-    q_summed = q_binned.bins.concat('spectrum')
+    # data = data.transform_coords("Q", graph=graph)
+    # q_boundaries = sc.concat([q_bins.min(), q_bins.max()], dim='Q')
 
-    denominator_bands = []
-    for i in range(number_of_wavelength_bands):
-        band = denominator['wavelength',
-                           wavelength_bands['wavelength',
-                                            i]:wavelength_bands['wavelength', i + 1]]
-        q_band = band.transform_coords("Q", graph=graph)
-        denominator_bands.append(sc.histogram(q_band, bins=q_bins))
-    denominator_q_summed = sc.concat(denominator_bands, 'wavelength').sum('spectrum')
+    # # TODO: once scipp-0.12 is out, we no longer need to move the attr into the coords
+    # data.bins.coords['wavelength'] = data.bins.attrs.pop('wavelength')
+
+    # q_binned = sc.bin(data, edges=[wavelength_bands, q_boundaries])
+    # q_summed = q_binned.bins.concat('spectrum')
+
+    # denominator_bands = []
+    # for i in range(number_of_wavelength_bands):
+    #     band = denominator['wavelength',
+    #                        wavelength_bands['wavelength',
+    #                                         i]:wavelength_bands['wavelength', i + 1]]
+    #     q_band = band.transform_coords("Q", graph=graph)
+    #     denominator_bands.append(sc.histogram(q_band, bins=q_bins))
+    # denominator_q_summed = sc.concat(denominator_bands, 'wavelength').sum('spectrum')
 
     # 4. Normalize =====================================================================
 
-    normalized = q_summed.bins / sc.lookup(func=denominator_q_summed, dim='Q')
-    if histogram_output:
-        normalized = sc.histogram(normalized, bins=q_bins)
-    if number_of_wavelength_bands == 1:
-        normalized = normalized['wavelength', 0]
+    # normalized = q_summed.bins / sc.lookup(func=denominator_q_summed, dim='Q')
+    normalized = _normalize(numerator=data_q, denominator=denominator_q, dim='Q')
+    # if histogram_output:
+    #     normalized = sc.histogram(normalized, bins=q_bins)
+    # if number_of_wavelength_bands == 1:
+    #     normalized = normalized['wavelength', 0]
     return normalized
