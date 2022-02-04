74e27aa065287f0a9863a85e7717128d1fb87c40
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-02-04 14:22:02 +0100

    Convert to correct dtypes

--- src/ess/powgen/beamline.py
+++ src/ess/powgen/beamline.py
@@ -1,11 +1,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+"""
+Beamline parameters and utilities for POWGEN.
+"""
+
 import scipp as sc
 
 
 def map_detector_to_spectrum(data: sc.DataArray, *,
                              detector_info: sc.DataArray) -> sc.DataArray:
-    if not sc.identical(data.coords['detector'], detector_info.coords['detector']):
+    if not sc.identical(
+            data.coords['detector'].to(dtype=detector_info.coords['detector'].dtype,
+                                       copy=False), detector_info.coords['detector']):
         raise sc.CoordError(
             "The 'detector' coords of `data` and `detector_info` do not match.")
 
@@ -13,7 +19,8 @@ def map_detector_to_spectrum(data: sc.DataArray, *,
     del out.coords['detector']
     # Add 1 because spectrum numbers in the data start at 1 but
     # detector_info contains spectrum indices which start at 0.
-    out.coords['spectrum'] = detector_info.coords['spectrum'] + sc.scalar(1,
-                                                                          dtype='int32')
+    out.coords['spectrum'] = (
+        detector_info.coords['spectrum'] +
+        sc.scalar(1, dtype=detector_info.coords['spectrum'].dtype))
 
     return out.rename_dims({'detector': 'spectrum'})
