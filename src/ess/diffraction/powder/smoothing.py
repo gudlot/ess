df51da581e1e6eefa95916fc523adb3295cc2dfa
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-02-04 11:34:12 +0100

    Remove smooth_data

--- src/ess/diffraction/powder/smoothing.py
+++ src/ess/diffraction/powder/smoothing.py
@@ -21,45 +21,6 @@ def _ensure_no_variances(var: sc.Variable) -> sc.Variable:
     return var
 
 
-def smooth_data(var: sc.Variable, *, dim: str, NPoints: int = 3) -> sc.Variable:
-    """
-    Function that smooths data by assigning the value of each point to the
-    mean of the values from surrounding points and itself. The number of points
-    to average is NPoint, which is odd so that the point itself is in the
-    center of the averaged interval. If an even NPoints is given, it is
-    incremented. At the ends of the interval the full number of points is not
-    used, but all available within NPoints//2 is.
-
-    Parameters
-    ----------
-        var: scipp variable
-            The variable which should have its values smoothed
-
-        dim: scipp Dim
-            The dimension along which values should be smoothed
-
-        NPoints: int
-            The number of points to use in the mean (odd number)
-    """
-
-    if NPoints < 3:
-        raise ValueError("smooth_data needs NPoints of 3 or higher.")
-
-    var = _ensure_no_variances(var)
-
-    data_length = var.sizes[dim]
-    out = var.copy()  # preallocate output variable
-
-    hr = NPoints // 2  # half range rounded down
-
-    for index in range(data_length):
-        begin = max(0, index - hr)
-        end = min(data_length, index + hr + 1)
-        out[dim, index] = sc.mean(var[dim, begin:end], dim)
-
-    return out
-
-
 def fft_smooth(var: sc.Variable, *, dim: str, order: int,
                Wn: sc.Variable) -> sc.Variable:
     var = _ensure_no_variances(var)
