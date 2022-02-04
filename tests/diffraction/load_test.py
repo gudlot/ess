efe79463c9f712648628208b4dac2b8754ac1b60
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-02-04 14:32:18 +0100

    Add load_test for diffraction

---
+++ tests/diffraction/load_test.py
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+import pytest
+
+from ess.diffraction import load_calibration
+from ess.powgen import data
+
+
+def test_load_calibration_loads_required_data():
+    loaded = load_calibration(
+        data.calibration_file(),
+        instrument_filename='POWGEN_Definition_2011-02-25.xml')
+
+    assert 'difa' in loaded
+    assert 'difc' in loaded
+    assert 'tzero' in loaded
+    assert 'mask' in loaded
+    assert 'detector' in loaded.coords
+    assert loaded.dims == ['detector']
+
+
+def test_load_calibration_requires_instrument_definition():
+    with pytest.raises(ValueError):
+        load_calibration(data.calibration_file())
+
+
+def test_load_calibration_can_only_have_1_instrument_definition():
+    with pytest.raises(ValueError):
+        load_calibration(data.calibration_file(),
+                         instrument_name='POWGEN',
+                         instrument_filename='POWGEN_Definition_2011-02-25.xml')
