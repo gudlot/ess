350420c76ca17cc6b506b022cd154e96339b1276
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-02-24 14:11:49 +0100

    Remove TODO comment

--- src/ess/diffraction/corrections.py
+++ src/ess/diffraction/corrections.py
@@ -128,7 +128,6 @@ def normalize_by_proton_charge(data: sc.DataArray,
                                *,
                                proton_charge: str = 'gd_prtn_chrg',
                                in_place: bool = False) -> sc.DataArray:
-    # TODO unit of proton charge?
     proton_charge = data.attrs[proton_charge]
     if in_place:
         data /= proton_charge
