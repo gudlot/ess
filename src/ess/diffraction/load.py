89f55381c3e0b925d3212c87cee7b098c023afef
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-02-24 14:09:57 +0100

    Only subtract empty instrument if its proton charge is alrge enough

--- src/ess/diffraction/load.py
+++ src/ess/diffraction/load.py
@@ -27,6 +27,19 @@ def _load_aux_file_as_wavelength(filename: Union[str, Path]) -> sc.DataArray:
                                })
 
 
+def _remove_based_on_proton_charge(da: sc.DataArray) -> bool:
+    charge = da.meta['proton_charge'].value.data
+    limit = da.meta['gd_prtn_chrg'].to(unit=charge.unit)
+    if (charge.max() < limit).value:
+        get_logger('diffraction').info(
+            'Discarding data for the empty instrument because its proton charge '
+            'is too low. Vanadium will not be corrected for background. '
+            'Maximum charge: %f%s vs gd_prtn_chrg: %f%s',
+            charge.max().value, charge.unit, limit.value, limit.unit)
+        return True
+    return False
+
+
 def load_and_preprocess_vanadium(
         vanadium_file: Union[str, Path],
         empty_instrument_file: Union[str, Path]) -> sc.DataArray:
@@ -38,15 +51,20 @@ def load_and_preprocess_vanadium(
     :param vanadium_file: File that contains the vanadium data.
     :param empty_instrument_file: File that contains data for the empty instrument.
                                   Must correspond to the same setup as `vanadium_file`.
-    :return: Vanadium - empty instrument with a wavelength coordinate.
+    :return: (Vanadium - empty instrument) with a wavelength coordinate.
     """
-    get_logger('diffraction').info(
-        'Loading vanadium from file %s\n'
-        'and correcting by empty instrument from file %s', vanadium_file,
-        empty_instrument_file)
+    get_logger('diffraction').info('Loading vanadium from file %s.', vanadium_file)
     vanadium = _load_aux_file_as_wavelength(vanadium_file)
-    empty_instrument = _load_aux_file_as_wavelength(empty_instrument_file)
     normalize_by_proton_charge(vanadium, in_place=True)
+    empty_instrument = _load_aux_file_as_wavelength(empty_instrument_file)
+    if _remove_based_on_proton_charge(empty_instrument):
+        # TODO This is a hack because in the POWGEN test data, all events are
+        #  filtered out for the empty instrument. Ultimately, we need to properly
+        #  filter pulses by proton charge when that functionality is available.
+        return vanadium
+    get_logger('diffraction').info(
+        'Subtracting empty instrument loaded from file %s from vanadium.',
+        empty_instrument_file)
     normalize_by_proton_charge(empty_instrument, in_place=True)
     return subtract_empty_instrument(vanadium, empty_instrument)
 
